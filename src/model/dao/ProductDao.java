8e37be1e9f29875d1964f91d98ebaa8a11287c27
Tiago Costa <tiagocostasantos0013@gmail.com>
2025-08-07 20:17:45 -0300

    insert ProductDao methods

--- src/model/dao/ProductDao.java
+++ src/model/dao/ProductDao.java
@@ -1,5 +1,17 @@
 package model.dao;
 
-public interface ProductDao {
+import java.util.List;
 
+import model.entities.Product;
+
+public interface ProductDao {
+	void insert(Product obj);
+
+	void update(Product obj);
+
+	void deleteById(Integer id);
+
+	Product findById(Integer id);
+
+	List<Product> findAll();
 }
