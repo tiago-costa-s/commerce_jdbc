2331c4fd3e0fdf38f64982dd270d865d2ad7e689
Tiago Costa <tiagocostasantos0013@gmail.com>
2025-08-07 20:24:21 -0300

    Implemented ProductDao

---
+++ src/model/dao/impl/ProductDaoJDBC.java
@@ -0,0 +1,40 @@
+package model.dao.impl;
+
+import java.util.List;
+
+import model.dao.ProductDao;
+import model.entities.Product;
+
+public class ProductDaoJDBC implements ProductDao {
+
+	@Override
+	public void insert(Product obj) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void update(Product obj) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void deleteById(Integer id) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Product findById(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Product> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
